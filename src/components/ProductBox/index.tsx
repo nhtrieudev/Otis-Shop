b8294c95c48a5feb0ffc600076ddae2ac86e20be
nhtrieudev <113841073+nhtrieudev@users.noreply.github.com>
2022-12-10 23:19:18 +0700

    Update index.tsx

--- src/components/ProductBox/index.tsx
+++ src/components/ProductBox/index.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import formatter from "../format-currency";
 
 function ProductBox(props: any) {
+  let navigate = useNavigate();
   return (
     <>
       {props.propProd.slice(0, 5).map((product: any, index: any) => (
         <div className="product-box" key={index}>
           <div className="product-box__thumb">
-            <img src={product.img} alt="" />
+            <img onClick={() => navigate(`/${product.name}`)} src={product.img} alt="" />
           </div>
           <div className="product-box__content">
             <Link to={`${product.name}`} className="product-box__content--name">
