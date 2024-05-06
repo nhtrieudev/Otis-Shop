15fbaf284778a5405bb5efa661f13a106f80f43c
nhtrieudev <113841073+nhtrieudev@users.noreply.github.com>
2024-05-06 23:08:43 +0700

    Update index.tsx index 0

--- src/components/Category/Product/index.tsx
+++ src/components/Category/Product/index.tsx
@@ -8,7 +8,7 @@ function ProductBox(props: any) {
       {props.propProd.map((product: any, index: any) => (
         <div className="product-box" key={index}>
           <div className="product-box__thumb">
-            <img onClick={() => navigate(`/${product.name}`)} src={product.img} alt="" />
+            <img onClick={() => navigate(`/${product.name}`)} src={product.img[0]} alt="" />
           </div>
           <div className="product-box__content">
             <Link
